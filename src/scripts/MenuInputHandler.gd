d803c6967ef5bebe036414d24432eb21f4932cfc
Dominik <hello@dviererbe.de>
2024-01-27 17:16:46 +0200

    feat: add credits scene + quit game functionality

--- src/scripts/MenuInputHandler.gd
+++ src/scripts/MenuInputHandler.gd
@@ -1,8 +1,17 @@
 extends Node
 
-
+const MENU_SCENE_PATH = "res://scenes/Menu.tscn"
+const CREDITS_SCENE_PATH = "res://scenes/Credits.tscn"
 const MAIN_SCENE_PATH = "res://scenes/Main.tscn"
-
 
 func _OnStartGameButtonPressed():
 	SceneSwitcher.SwitchToScene(MAIN_SCENE_PATH)
+
+func _OnShowCreditsButtonPressed():
+	SceneSwitcher.SwitchToScene(CREDITS_SCENE_PATH)
+	
+func _OnBackButtonPressed():
+	SceneSwitcher.SwitchToScene(MENU_SCENE_PATH)
+		
+func _OnCloseGameButtonPressed():
+	get_tree().quit()
