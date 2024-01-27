f7106c3b782c0364fa2e2e46ef04070d23407c7e
Dominik <hello@dviererbe.de>
2024-01-27 15:34:14 +0200

    feat: add initial player scene

---
+++ src/scripts/PlayerInputHandler.gd
@@ -0,0 +1,36 @@
+extends RigidBody2D
+
+
+@export var MovementSpeed = 400
+
+var CurrentInputHandler = DefaultInputHandler;
+
+func _process(delta):
+	CurrentInputHandler.call(delta)
+	
+func DefaultInputHandler(delta):
+	if Input.is_action_just_pressed("OpenMap"):
+		print("Open Map")
+		# TODO: open map
+		CurrentInputHandler = MapOpenInputHandler
+		return
+	
+	var direction = Input.get_vector(
+		"WalkLeft",  # negative X
+		"WalkRight", # positive X
+		"WalkUp",    # negative Y
+		"WalkDown")  # positive Y 
+	
+	var velocity = MovementSpeed * direction;
+	
+	if velocity.length() > 0:
+		position += velocity * delta
+		# TODO: set animation to walking
+	else:
+		pass # TODO: set animation to standing
+
+func MapOpenInputHandler(delta):
+	if Input.is_action_just_pressed("OpenMap"):
+		print("Close Map")
+		# TODO: close map
+		CurrentInputHandler = DefaultInputHandler
