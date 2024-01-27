ade5bf7ce92c331b0dc44c0a9c4f2ca719937db2
Dominik <hello@dviererbe.de>
2024-01-27 15:47:03 +0200

    refactor: add helper functions

--- src/scripts/PlayerInputHandler.gd
+++ src/scripts/PlayerInputHandler.gd
@@ -5,11 +5,21 @@ extends RigidBody2D
 
 var CurrentInputHandler = DefaultInputHandler;
 
+
+func IsPrimaryActionJustPressed():
+	return Input.is_action_just_pressed("PrimaryAction")
+
+
+func IsSecondaryActionJustPressed():
+	return Input.is_action_just_pressed("SecondaryAction")
+
+
 func _process(delta):
 	CurrentInputHandler.call(delta)
-	
+
+
 func DefaultInputHandler(delta):
-	if Input.is_action_just_pressed("SecondaryAction"):
+	if IsSecondaryActionJustPressed():
 		print("Open Map")
 		# TODO: open map
 		CurrentInputHandler = MapOpenInputHandler
@@ -29,8 +39,9 @@ func DefaultInputHandler(delta):
 	else:
 		pass # TODO: set animation to standing
 
+
 func MapOpenInputHandler(delta):
-	if Input.is_action_just_pressed("SecondaryAction"):
+	if IsSecondaryActionJustPressed():
 		print("Close Map")
 		# TODO: close map
 		CurrentInputHandler = DefaultInputHandler
